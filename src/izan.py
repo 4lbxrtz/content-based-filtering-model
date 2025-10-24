027b1966d38269b1d8dd1b485e0b39bfa120d6aa
Izan Perez Gonzalez <alu0101543345@ull.edu.es>
2025-10-24 09:42:56 +0100

    feat: izan

---
+++ src/izan.py
@@ -0,0 +1,24 @@
+def term_index():
+  pass
+
+
+def term():
+  pass
+
+def tf(word, document):
+  pass
+
+def openDoc(documentName):
+  try:
+    dfile = open(documentName)
+    return d
+
+  except Exception:
+    exit(NameError)
+
+def buildTable(docName, wordsDocName, lemDocName):
+  doc = open(docName)
+  wordsDoc = open(wordsDocName)
+  lemDoc = open(lemDocName)
+
+  
