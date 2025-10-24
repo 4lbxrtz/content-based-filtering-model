4307b868dc8f578e7573947257b2775c0a951962
Izan Perez Gonzalez <alu0101543345@ull.edu.es>
2025-10-24 10:08:57 +0100

    feat: lematizar

--- src/izan.py
+++ src/izan.py
@@ -11,14 +11,35 @@ def tf(word, document):
 def openDoc(documentName):
   try:
     dfile = open(documentName)
-    return d
+    return 1
 
   except Exception:
     exit(NameError)
 
-def buildTable(docName, wordsDocName, lemDocName):
-  doc = open(docName)
-  wordsDoc = open(wordsDocName)
-  lemDoc = open(lemDocName)
+import re
+def getWords(docName):
+  with open(docName, "r", encoding="utf-8") as file:
+    text = file.read()
+  return re.findall(r'\b\w+\b', text.lower())
+
+def deleteStopWords(words, docName):
+  with open(docName, "r", encoding="utf-8") as file:
+    stopWords = [line.strip() for line in file if line.strip()]
+  return [word for word in words if word not in stopWords]
+
+import json
+def lematiceWords(words, docName):
+  with open(docName, "r", encoding="utf-8") as file:
+    lemat = json.load(file)
+  return [lemat.get(word, word) for word in words]
+
+
+def buildTable(docName, stopWordsDocName, lemDocName):
+  words = getWords(docName)
+  words = deleteStopWords(words, stopWordsDocName)
+  words = lematiceWords(words, lemDocName)
+  
+
 
   
+
